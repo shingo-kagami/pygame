d48db9cc5a1f4129d820924378f7c4fe5558b8d6
René Dudfield <renesd@gmail.com>
2021-08-15 12:01:47 +0200

    manylinux: Upgrade other dependencies

--- buildconfig/manylinux-build/docker_base/pulseaudio/build-pulseaudio.sh
+++ buildconfig/manylinux-build/docker_base/pulseaudio/build-pulseaudio.sh
@@ -2,7 +2,7 @@
 set -e -x
 
 cd /pulseaudio_build/
-PULSEFILE="pulseaudio-14.0"
+PULSEFILE="pulseaudio-15.0"
 
 curl -sL https://www.freedesktop.org/software/pulseaudio/releases/${PULSEFILE}.tar.xz > ${PULSEFILE}.tar.xz
 sha512sum -c pulseaudio.sha512
